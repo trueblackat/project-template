fad9bdc3ce23bbcb2dce372f1d7be427a8afc36e
Amir Gafurov <gafurovamir@gmail.com>
2016-12-22 11:29:14 +0400

    update Foundation to 6.3.0 + fixes

--- src/js/vendors.js
+++ src/js/vendors.js
@@ -1,38 +1,38 @@
 // Foundation Core files
+// ../../bower_components/foundation-sites/dist/js/plugins/foundation.core.js
 
 // Если в проекте используются встроенные js-плагины от Foundation, заменить // на //=
 
-// ../../bower_components/foundation-sites/dist/plugins/foundation.core.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.box.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.keyboard.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.mediaQuery.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.motion.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.nest.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.timerAndImageLoader.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.touch.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.util.triggers.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.box.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.keyboard.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.mediaQuery.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.motion.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.nest.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.timerAndImageLoader.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.touch.js
+// ../../bower_components/foundation-sites/dist/js/foundation.util.triggers.js
 
 // Foundation plugins
 
-// ../../bower_components/foundation-sites/dist/plugins/foundation.abide.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.accordion.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.accordionMenu.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.drilldown.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.dropdown.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.dropdownMenu.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.equalizer.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.interchange.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.magellan.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.offcanvas.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.orbit.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.responsiveMenu.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.responsiveToggle.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.reveal.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.slider.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.sticky.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.tabs.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.toggler.js
-// ../../bower_components/foundation-sites/dist/plugins/foundation.tooltip.js
+// ../../bower_components/foundation-sites/dist/js/foundation.abide.js
+// ../../bower_components/foundation-sites/dist/js/foundation.accordion.js
+// ../../bower_components/foundation-sites/dist/js/foundation.accordionMenu.js
+// ../../bower_components/foundation-sites/dist/js/foundation.drilldown.js
+// ../../bower_components/foundation-sites/dist/js/foundation.dropdown.js
+// ../../bower_components/foundation-sites/dist/js/foundation.dropdownMenu.js
+// ../../bower_components/foundation-sites/dist/js/foundation.equalizer.js
+// ../../bower_components/foundation-sites/dist/js/foundation.interchange.js
+// ../../bower_components/foundation-sites/dist/js/foundation.magellan.js
+// ../../bower_components/foundation-sites/dist/js/foundation.offcanvas.js
+// ../../bower_components/foundation-sites/dist/js/foundation.orbit.js
+// ../../bower_components/foundation-sites/dist/js/foundation.responsiveMenu.js
+// ../../bower_components/foundation-sites/dist/js/foundation.responsiveToggle.js
+// ../../bower_components/foundation-sites/dist/js/foundation.reveal.js
+// ../../bower_components/foundation-sites/dist/js/foundation.slider.js
+// ../../bower_components/foundation-sites/dist/js/foundation.sticky.js
+// ../../bower_components/foundation-sites/dist/js/foundation.tabs.js
+// ../../bower_components/foundation-sites/dist/js/foundation.toggler.js
+// ../../bower_components/foundation-sites/dist/js/foundation.tooltip.js
 
 // Other vendor
 //= ../../bower_components/promise-polyfill/promise.min.js
